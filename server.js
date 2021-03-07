6f4fecc51dbcab5194f5358bf22429df03845408
Rasbakimo <bakimocleckley@yahoo.com>
2021-03-07 17:52:11 -0500

    routes added

--- server.js
+++ server.js
@@ -1,75 +1,24 @@
 
-const express = require("express");
-const path = require("path");
-const fs = require('fs');
-const util = require('util');
+// Require the Express
+var express = require("express");
 
-// creating an "express" server
-const app = express();
+// Create an express server.
+var app = express();
 
-// port that will be used
-const PORT = process.env.PORT || 8000;
+// Set port.
+var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
+// Set up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "./public")));
 
-//Set variables
-const writefileAsync = util.promisify(fs.writeFile);
-const readFileAsync = util.promisify(fs.readFile);
-let allNotes;
+// ROUTES
+// Point the server to the route files.
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-// ROUTER
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "./Deveolp/public/notes.html"));
-});
-
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
-});
-
-app.get("/api/notes", function (req, res) {
-    readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
-        .then(function (data) {
-            return res.json(JSON.parse(data));
-        });
-});
-
-app.post("/api/notes", function (req, res) {
-    var newNote = req.body;
-    readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
-        .then(function (data) {
-            allNotes = JSON.parse(data);
-            if (newNote.id || newNote.id === 0) {
-                let currNote = allNotes[newNote.id];
-                currNote.title = newNote.title;
-                currNote.text = newNote.text;
-            } else {
-                allNotes.push(newNote);
-            }
-            writefileAsync(path.join(__dirname, "./db/db.json"), JSON.stringify(allNotes))
-                .then(function () {
-                    console.log("Wrote db.json");
-                })
-        });
-    res.json(newNote);
-});
-
-app.delete("/api/notes/:id", function (req, res) {
-    var id = req.params.id;
-    readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
-        .then(function (data) {
-            allNotes = JSON.parse(data);
-            allNotes.splice(id, 1);
-            writefileAsync(path.join(__dirname, "./db/db.json"), JSON.stringify(allNotes))
-                .then(function () {
-                    console.log("Deleted db.json");
-                })
-        });
-    res.json(id);
-});
 // LISTENER
-app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
+// Listen on port.
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });
