9919f67badf47f717c18a8141a12ef49ee9acb1c
Rasbakimo <bakimocleckley@yahoo.com>
2021-03-07 16:09:11 -0500

    server listening

--- server.js
+++ server.js
@@ -1,24 +1,75 @@
 
-// npm packages for functionality
+const express = require("express");
+const path = require("path");
 const fs = require('fs');
-const path = require('path');
-const express = require('express');
+const util = require('util');
 
-//  creating an "express" server
+// creating an "express" server
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+// port that will be used
+const PORT = process.env.PORT || 8000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "./public")));
+
+//Set variables
+const writefileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
+let allNotes;
 
 // ROUTER
-require('./routes/htmlRoutes')(app);
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./Deveolp/public/notes.html"));
+});
 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
 
+app.get("/api/notes", function (req, res) {
+    readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
+        .then(function (data) {
+            return res.json(JSON.parse(data));
+        });
+});
+
+app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
+    readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
+        .then(function (data) {
+            allNotes = JSON.parse(data);
+            if (newNote.id || newNote.id === 0) {
+                let currNote = allNotes[newNote.id];
+                currNote.title = newNote.title;
+                currNote.text = newNote.text;
+            } else {
+                allNotes.push(newNote);
+            }
+            writefileAsync(path.join(__dirname, "./db/db.json"), JSON.stringify(allNotes))
+                .then(function () {
+                    console.log("Wrote db.json");
+                })
+        });
+    res.json(newNote);
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+    var id = req.params.id;
+    readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
+        .then(function (data) {
+            allNotes = JSON.parse(data);
+            allNotes.splice(id, 1);
+            writefileAsync(path.join(__dirname, "./db/db.json"), JSON.stringify(allNotes))
+                .then(function () {
+                    console.log("Deleted db.json");
+                })
+        });
+    res.json(id);
+});
 // LISTENER
-
-app.listen(PORT, () => {
-  console.log(`App listening on PORT: ${PORT}`);
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
 });
