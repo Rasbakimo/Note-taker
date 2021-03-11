6661a46e887f126f889631c1803ce6c020623b36
Rasbakimo <bakimocleckley@yahoo.com>
2021-03-11 13:17:48 -0500

    route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,6 +6,9 @@ module.exports = function(app){
     app.get("/notes", function(req, res){
         res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
     });
+    app.get("/", function(req, res) {
+        res.json(path.join(__dirname, "../Develop?public/index.html"));
+      });
 
     // Route to go to index page
     app.get("*", function(req, res){
